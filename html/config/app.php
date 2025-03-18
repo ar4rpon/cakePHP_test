fee5a9af1ee65e7535fba615b09c357bbc3f1f88
ar4rpon <hsgw28tech@gmail.com>
2025-03-18 15:35:09 +0900

    add:feature/articles

--- html/config/app.php
+++ html/config/app.php
@@ -297,6 +297,7 @@ return [
             'driver' => Mysql::class,
             'persistent' => false,
             'timezone' => 'UTC',
+            'encoding' => 'utf8mb4',
 
             /*
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
